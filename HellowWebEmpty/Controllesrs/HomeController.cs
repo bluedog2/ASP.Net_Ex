91ecf6f46acbba441d60ce06cafc1fa653054f0b
KimsangEun <zlwbgo@naver.com>
2024-04-28 22:23:08 +0900

    ASP1차

---
+++ HellowWebEmpty/Controllesrs/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using HellowWebEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HellowWebEmpty.Controllesrs
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "welcome to asp.net MVC"
+            };
+
+            ViewBag.Noti = "Input message and click";
+
+            return View(msg);
+        }
+
+        //Post처리
+        [HttpPost]
+        public IActionResult Index(HelloMessage obj)
+        {
+            ViewBag.Noti = "Message Changed";
+            return View(obj);
+        }
+    }
+}
