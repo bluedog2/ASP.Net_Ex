f185643ffadf7b22de480aec3c5d60f090575cb6
KimsangEun <zlwbgo@naver.com>
2024-04-29 22:47:08 +0900

    asp2차 razor

--- HellowWebEmpty/Startup.cs
+++ HellowWebEmpty/Startup.cs
@@ -13,13 +13,18 @@ namespace HellowWebEmpty
     // M (¸ðµ¨)
     // V (ºä)
     // C (ÄÁÆ®·Ñ·¯)
+
+    // M 
+    // VC
+    // M - V - VM
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,10 +39,7 @@ namespace HellowWebEmpty
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}"
-                );
+                endpoints.MapRazorPages();
             });
         }
     }
